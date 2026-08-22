ab062eb60f55f8ee27fe31be0c809e71ca371d0a
binvahub <binvahub@gmail.com>
2026-08-22 19:04:41 +0330

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BinvaApp from './BinvaApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BinvaApp />
+  </React.StrictMode>,
+)
