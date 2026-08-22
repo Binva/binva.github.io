b11172982aedafeae8912944fe1d25dca0b0030b
binvahub <binvahub@gmail.com>
2026-08-22 18:54:47 +0330

    Delete .github/workflows/src/main.jsx

--- .github/workflows/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BinvaApp from './BinvaApp.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BinvaApp />
-  </React.StrictMode>,
-)
