d3bb19e34d8e9a03ba702babb2ac30c4c4ce44b2
Binva <binvahub@gmail.com>
2026-08-25 22:07:53 +0330

    Update BinvaApp.jsx

--- src/BinvaApp.jsx
+++ src/BinvaApp.jsx
@@ -656,7 +656,7 @@ export default function BinvaApp() {
       </div>
 
       <div className="binessa-mobile-bar" style={{
-        display: "none", position: "fixed", top: 0, left: 0, right: 0, zIndex: 20,
+        display: "none", position: "fixed", top: 0, left: 0, right: 0, zIndex: 35,
         background: T.surface, borderBottom: `1px solid ${T.borderSoft}`, padding: "12px 16px",
         alignItems: "center", justifyContent: "space-between",
       }}>
